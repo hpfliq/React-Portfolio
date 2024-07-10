a31851434c7e594d52afed8b74d03963d34acf8d
hpfliq <hpfliq@gmail.com>
2024-07-10 18:09:54 +0530

    g

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,13 +7,13 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/hpfliq",
   resume:
     "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+  linkedin: "https://www.linkedin.com/in/hpfliq/",
+  twitter: "https://x.com/hpfliq",
+  insta: "https://www.instagram.com/hpfliq/",
+  facebook: "https://www.facebook.com/hpfliq/",
 };
 
 export const skills = [
