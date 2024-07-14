09af0484c307d1bd0faee51e868a14252be4eac7
hpfliq <hpfliq@gmail.com>
2024-07-14 15:52:41 +0530

    h

--- src/data/constants.js
+++ src/data/constants.js
@@ -148,7 +148,7 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://drive.google.com/file/d/1pY0l92M6XYS7iTjngYn0tv3NtMhu5h8c/view?id={ID}&sz=w1000",
+    img: "https://drive.usercontent.google.com/download?id=1pY0l92M6XYS7iTjngYn0tv3NtMhu5h8c&export=view&authuser=1",
     role: "Graphic Designer & Video Editor",
     company: "Groarz Digital Marketing Agency, Nagercoil",
     date: "Oct 2023 - Feb 2024",
